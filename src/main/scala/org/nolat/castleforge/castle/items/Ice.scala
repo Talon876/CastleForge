a8acaddc47d7287d7d97e8e802b99aff6d62336f
Talon Daniels <talondaniels@gmail.com>
2012-10-12 16:20:53 -0600

    created attributes

--- src/main/scala/org/nolat/castleforge/castle/items/Ice.scala
+++ src/main/scala/org/nolat/castleforge/castle/items/Ice.scala
@@ -6,7 +6,7 @@ import org.nolat.castleforge.graphics.Sprite
 class Ice extends Item with RNG {
 
   sprite = new Sprite(getItemType)
-  sprite.setRandomAnimation(List(.33f, .33f, .33f, .01f))
+  sprite.setRandomAnimation()
 
-  override def getItemType = Sprites.floor
+  override def getItemType = Sprites.ice
 }
---
+++ src/main/scala/org/nolat/castleforge/castle/items/attributes/attributes.scala
@@ -0,0 +1,79 @@
+package org.nolat.castleforge.castle.items.attributes
+
+import org.newdawn.slick.Color
+import javax.swing.JFrame
+
+abstract class Attribute {}
+
+object IDColor {
+  val WHITE = Color.white
+  val BLACK = Color.black
+  val RED = Color.red
+  val ORANGE = Color.orange
+  val YELLOW = Color.yellow
+  val GREEN = Color.green
+  val BLUE = Color.blue
+  val PURPLE = new Color(208, 27, 224)
+}
+trait IDColor extends Attribute {
+  var idcolor = IDColor.WHITE
+}
+
+object Quantity {
+  val ZERO = 0
+  val ONE = 1
+  val THREE = 3
+  val FIVE = 5
+  val TEN = 10
+  val FIFTEEN = 15
+  val TWENTY = 20
+}
+trait Quantity extends Attribute {
+  var quantity = Quantity.ZERO
+}
+
+object Shape {
+  val DIAMOND = "diamond"
+  val PENTAGON = "pentagon"
+  val TRIANGLE = "triangle"
+  val STAR = "star"
+}
+trait Shape extends Attribute {
+  var shape = Shape.DIAMOND
+}
+
+object Luminosity {
+  val OFF = 0
+  val LOW = 3
+  val MEDIUM = 7
+  val HIGH = 15
+}
+trait Luminosity extends Attribute {
+  var luminosity = Luminosity.OFF
+}
+
+object Direction {
+  val NORTH = scala.math.toRadians(90).toFloat
+  val WEST = scala.math.toRadians(180).toFloat
+  val SOUTH = scala.math.toRadians(270).toFloat
+  val EAST = scala.math.toRadians(0).toFloat
+}
+trait Direction extends Attribute {
+  var direction = Direction.NORTH
+}
+
+object TorchState {
+  val ON = true
+  val OFF = false
+}
+trait TorchState {
+  var torchstate = TorchState.ON
+}
+
+object CheckpointState {
+  val ACTIVE = true
+  val INACTIVE = false
+}
+trait CheckpointState extends Attribute {
+  var checkpointstate = CheckpointState.INACTIVE
+}
