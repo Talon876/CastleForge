0b506640b9cac4b7d791a1b51ba747a141b064c5
Talon Daniels <talondaniels@gmail.com>
2012-10-22 16:32:11 -0600

    began implementation of Player

---
+++ src/main/scala/org/nolat/castleforge/castle/Player.scala
@@ -0,0 +1,51 @@
+package org.nolat.castleforge.castle
+
+import org.nolat.castleforge.graphics.Renderable
+import org.newdawn.slick.state.StateBasedGame
+import org.newdawn.slick.GameContainer
+import org.newdawn.slick.Graphics
+import org.newdawn.slick.Input
+import org.newdawn.slick.geom.Vector2f
+import org.nolat.castleforge.Config
+import org.nolat.castleforge.graphics.Sprites
+import org.nolat.castleforge.castle.items.GameItem
+import org.nolat.castleforge.graphics.Sprite
+import org.newdawn.slick.Color
+
+class Player extends GameItem {
+
+  object PlayerState extends Enumeration {
+    type PlayerState = Value
+    val WALKING_UP, WALKING_DOWN, WALKING_RIGHT, WALKING_LEFT, IDLE = Value
+  }
+
+  position = new Vector2f(8, 8)
+  sprite = new Sprite(Sprites.player)
+  sprite.setAnimation("idle")
+
+  var state = PlayerState.IDLE
+
+  val speed = .2f
+
+  override def update(container: GameContainer, game: StateBasedGame, delta: Int) {
+    if (state == PlayerState.IDLE) {
+      if (container.getInput().isKeyDown(Input.KEY_W)) {
+        position.y -= 1 * speed
+        sprite.setAnimation("walking_up")
+      } else if (container.getInput().isKeyDown(Input.KEY_S)) {
+        position.y += 1 * speed
+        sprite.setAnimation("walking_down")
+      } else if (container.getInput().isKeyDown(Input.KEY_A)) {
+        position.x -= 1 * speed
+        sprite.setAnimation("walking_left")
+      } else if (container.getInput().isKeyDown(Input.KEY_D)) {
+        position.x += 1 * speed
+        sprite.setAnimation("walking_right")
+      }
+    }
+  }
+
+  override def render(container: GameContainer, game: StateBasedGame, g: Graphics) {
+    sprite.getAnimation.draw(position.x.toInt, position.y.toInt, Color.white)
+  }
+}
--- src/main/scala/org/nolat/castleforge/states/ExperimentScreen2.scala
+++ src/main/scala/org/nolat/castleforge/states/ExperimentScreen2.scala
@@ -19,6 +19,8 @@ import java.io.File
 import org.nolat.castleforge.castle.Floor
 import org.nolat.castleforge.castle.items.Item
 import org.newdawn.slick.state.transition.EmptyTransition
+import org.nolat.castleforge.castle.Player
+
 object ExperimentScreen2 {
   val ID = 4
 }
@@ -29,33 +31,25 @@ class ExperimentScreen2 extends BasicGameState {
 
   lazy val castle = MapLoad.loadMap(new File("/Users/talon/.castleforge/maps/Allitems.xml"), false)
 
-  var player = (3, 3)
+  var player: Player = null
 
   override def init(container: GameContainer, game: StateBasedGame) {
     this.game = game
-
+    this.player = new Player
   }
 
   override def update(container: GameContainer, game: StateBasedGame, delta: Int) {
     castle.update(container, game, delta)
-
-    if (container.getInput().isKeyPressed(Input.KEY_W)) {
-      player = (player._1 + 0, player._2 - 1)
-    } else if (container.getInput().isKeyPressed(Input.KEY_S)) {
-      player = (player._1 + 0, player._2 + 1)
-    } else if (container.getInput().isKeyPressed(Input.KEY_A)) {
-      player = (player._1 - 1, player._2 - 0)
-    } else if (container.getInput().isKeyPressed(Input.KEY_D)) {
-      player = (player._1 + 1, player._2 - 0)
-    }
+    player.update(container, game, delta)
   }
 
   override def render(container: GameContainer, game: StateBasedGame, g: Graphics) {
     g.setBackground(Color.black)
     g.setColor(Color.white)
     castle.render(container, game, g)
-    g.setColor(Color.green)
-    g.drawRect(player._1 * Config.TileWidth + Config.TileOffsetX, player._2 * Config.TileHeight + Config.TileOffsetY, 64, 64)
+    player.render(container, game, g)
+    //g.setColor(Color.green)
+    //g.drawRect(player._1 * Config.TileWidth + Config.TileOffsetX, player._2 * Config.TileHeight + Config.TileOffsetY, 64, 64)
   }
 
   override def keyReleased(key: Int, c: Char) {
